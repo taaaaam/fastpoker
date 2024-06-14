b638b46d82eb3b6d22ff6362a5229a009237576d
Tam Vu <tam.vu@yale.edu>
2024-06-14 15:20:12 -0400

    connection attempt

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
+import dotenv from "dotenv";
 
+dotenv.config({path: 'config.env'});
 const PORT = process.env.PORT || 5050;
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
