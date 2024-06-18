67a48d545196f25e64ab0b8baba2952548f25a00
Tam Vu <tam.vu@yale.edu>
2024-06-17 21:56:45 -0400

    super edit

--- server/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-// Sample user controller
-exports.getAllUsers = (req, res) => {
-    res.json([
-      { id: 1, name: 'John Doe' },
-      { id: 2, name: 'Jane Doe' }
-    ]);
-  };
-  
--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-// Define routes
-router.get('/users', userController.getAllUsers);
-
-module.exports = router;
